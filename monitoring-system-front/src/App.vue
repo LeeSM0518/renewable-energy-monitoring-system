9cc7968aae38be391bce4f6dd21612654a009cdb
Sangmin Lee <nalsm98@naver.com>
2020-08-20 22:34:53 +0900

    모니터링 시스템 프론트 서버 구현중

---
+++ monitoring-system-front/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-app-bar app dense color="blue-grey darken-4" dark>
+      <v-toolbar-title>
+        <h3>HARANG MONITORING SYSTEM</h3>
+      </v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  components: {},
+
+  data: () => ({
+    //
+  }),
+
+  created() {},
+};
+</script>
