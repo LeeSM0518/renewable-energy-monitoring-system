65944c35442b743e33e8b3adeb8853649449177b
Sangmin Lee <nalsm98@naver.com>
2020-08-24 19:50:48 +0900

    홈페이지 구현 완료

--- monitoring-system-front/src/views/Home.vue
+++ monitoring-system-front/src/views/Home.vue
@@ -1,14 +1,54 @@
 <template>
-  <v-container fluid>
+  <v-container>
+    <v-row class="mt-7" justify="center">
+      <v-col cols="10">
+        <v-card outlined>
+          <v-list-item three-line>
+            <v-list-item-content>
+              <v-list-item-title class="headline mb-1"
+                >실시간 데이터</v-list-item-title
+              >
+              <v-list-item-subtitle
+                >1초 주기의 실시간 데이터를 볼 수 있다.</v-list-item-subtitle
+              >
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="blue-grey darken-4"
+              class="white--text"
+              @click="pushLive"
+              >View</v-btn
+            >
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
     <v-row justify="center">
-      <v-col cols="12">
-        <v-card elevation="10" dense class="blue-grey darken-4" dark>
-          <v-card-title><strong>PV 전압</strong></v-card-title>
-          <v-card-text class="white primary--text">
-            <v-col cols="12">
-              <Chart class="pa-1" :options="pvVoltageChartOptions"></Chart>
-            </v-col>
-          </v-card-text>
+      <v-col cols="10">
+        <v-card outlined>
+          <v-list-item three-line>
+            <v-list-item-content>
+              <v-list-item-title class="headline mb-1"
+                >축적 데이터</v-list-item-title
+              >
+              <v-list-item-subtitle
+                >1시간 주기의 24시간 데이터를 볼 수 있다.</v-list-item-subtitle
+              >
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="blue-grey darken-4"
+              class="white--text"
+              @click="pushHistory"
+              >View</v-btn
+            >
+          </v-card-actions>
         </v-card>
       </v-col>
     </v-row>
@@ -16,69 +56,16 @@
 </template>
 
 <script>
-import { Chart } from 'highcharts-vue';
-
-let min = 0;
-let max = 60;
-let plus = 1;
-
-let xList = [];
-for (let i = min; i <= max; i += plus) {
-  xList.push(i);
-}
-
-function randomList() {
-  let randomList = [];
-  for (let i = min; i <= max; i += plus) {
-    randomList.push(Math.floor(Math.random() * 100 + 1));
-  }
-  return randomList;
-}
-
 export default {
-  name: 'Home',
-  components: {
-    Chart,
-  },
-  data: () => ({
-    pvVoltageChartOptions: {
-      chart: {
-        type: 'line',
-      },
-      title: {
-        text: null,
-      },
-      subtitle: {
-        text: null,
-      },
-      xAxis: {
-        title: {
-          text: `<strong>m(minute)</strong>`,
-        },
-        categories: xList,
-      },
-      yAxis: {
-        title: {
-          text: `<strong>Voltage(V)</strong>`,
-        },
-        max: 100,
-        min: 0,
-      },
-      plotOptions: {
-        line: {
-          dataLabels: {
-            enabled: false,
-          },
-          enableMouseTracking: false,
-        },
-      },
-      series: [
-        {
-          name: '전압',
-          data: randomList(),
-        },
-      ],
+  methods: {
+    pushLive() {
+      this.$router.push('/live');
     },
-  }),
+    pushHistory() {
+      this.$router.push('/history');
+    },
+  },
 };
 </script>
+
+<style></style>
